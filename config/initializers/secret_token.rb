856c719f0cfb03a004eea0773901aa99855727db
poc7667 <poc7667@gmail.com>
2014-04-17 16:11:55 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DqaStreesfulServer::Application.config.secret_key_base = '6164ddd56a09e5e7e75dbbd5b2e91089e9c496bde22ed73639d803c35e2cd3dda38b256247d25f4b2785f38fbac49c8e904dd11e07b5c473eab93f4e5ec3706e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DqaStreesfulServer::Application.config.session_store :cookie_store, key: '_dqa_streesful_server_session'
