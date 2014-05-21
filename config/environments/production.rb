69fe6c4c6a3cb920297ab2ee894cca4cf0fecd6f
poc7667 <poc7667@gmail.com>
2014-05-21 13:46:25 +0800

    Fix: to compile all assets in not formal folder
    Signed-off-by: poc7667 <poc7667@gmail.com>

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,13 +20,14 @@ DqaStreesfulServer::Application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = false
+  config.serve_static_assets = true
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
+  # we won't want live compile it
   config.assets.compile = false
 
   # Generate digests for assets URLs.
@@ -59,7 +60,15 @@ DqaStreesfulServer::Application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w( bootstrap.css )
+  # config.assets.precompile += %w( vendor/themes/* )
+  config.assets.precompile += %w( vendor/themes/**/application.js )
+  config.assets.precompile += %w( vendor/themes/**/application.css )
+  # config.assets.precompile += %w[*.png *.jpg *.jpeg *.gif] 
+
+  # Precompile *all* assets, except those that start with underscore
+  config.assets.precompile << /(^[^_\/]|\/[^_])[^\/]*$/
+
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
